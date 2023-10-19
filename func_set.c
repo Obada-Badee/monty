4880c0791c2bf22ade039605e8b3d36aeea5804e
Obada-Badee <obadabadee3@hotmail.com>
2023-10-19 22:41:47 +0000

    complete the push op

--- func_set.c
+++ func_set.c
@@ -9,6 +9,43 @@
  */
 void push(stack_t **stack, unsigned int line_num)
 {
+	stack_t *new, *tmp;
+
+	if (opi_tok[1] == NULL)
+	{
+		argument_not_exist(line_number);
+		return;
+	}
+	if (!is_atoi())
+	{
+		argument_not_exist(line_number);
+		return;
+	}
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		(malloc_err());
+		return;
+	}
+	new->n = atoi(opi_tok[1]);
+	if ((*stack)->n == STACK)
+	{
+		tmp = (*stack)->next;
+		new->next = tmp;
+		new->prev = *stack;
+		if (tmp)
+			(tmp)->prev = new;
+		(*stack)->next = new;
+	}
+	else
+	{
+		tmp = *stack;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+		new->next = NULL;
+		new->prev = tmp;
+	}
 }
 
 
